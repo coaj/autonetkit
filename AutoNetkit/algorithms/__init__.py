8937c54c3d6f91320c0bcb87ece1f96e1ea70765
askar jaboldinov <a.jaboldinov@lboro.ac.uk>
2012-04-12 20:04:26 +0100

    init

--- AutoNetkit/algorithms/__init__.py
+++ AutoNetkit/algorithms/__init__.py
@@ -1,6 +1,7 @@
 from AutoNetkit.algorithms.autonomous_system import *
 from AutoNetkit.algorithms.bgp import *
 from AutoNetkit.algorithms.bgp_policy import *
+from AutoNetkit.algorithms.rpki import *
 from AutoNetkit.algorithms.dns import *
 from AutoNetkit.algorithms.graph_product import *
 from AutoNetkit.algorithms.housekeeping import *
@@ -10,6 +11,7 @@ from AutoNetkit.algorithms.naming import *
 import AutoNetkit.algorithms.autonomous_system 
 import AutoNetkit.algorithms.bgp 
 import AutoNetkit.algorithms.bgp_policy
+import AutoNetkit.algorithms.rpki
 import AutoNetkit.algorithms.dns 
 import AutoNetkit.algorithms.graph_product
 import AutoNetkit.algorithms.housekeeping
